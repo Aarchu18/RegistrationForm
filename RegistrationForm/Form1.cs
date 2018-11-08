3a29db5588df9da556c217b195baee6f6e62e1c4
Archana <archana.kumari@kelltontech.com>
2018-11-08 13:01:49 +0530

    first commit

---
+++ RegistrationForm/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace RegistrationForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=KELLGGNCPU0229\SQLEXPRESS;Initial Catalog=FormData;Integrated Security=True");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string query = "INSERT INTO FormTable (Name,PhoneNo,EmailId,Address) VALUES('"+textBox1.Text+ "', '" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "')";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            sda.SelectCommand.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("SUBMITTED SUCCESSFULLY!!!!!");
+
+       
+        }
+    }
+}
